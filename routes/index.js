ab14476bc65706506c710aed1c688569585ce27c
Jens Tinfors <jens.tinfors@gmail.com>
2017-09-05 08:45:16 +0200

    last minute fixes.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	var familyMembers = [
+		{ num: 1, textual: 'En' },
+		{ num: 2, textual: 'Två' },
+		{ num: 3, textual: 'Tre' },
+		{ num: 4, textual: 'Fyra' },
+		{ num: 5, textual: 'Fem' },
+	]
+	res.render('index', { familyMembers: familyMembers, title: 'Tinfors Fastigheter' });
 });
 
 router.post('/', function(req, res, next) {

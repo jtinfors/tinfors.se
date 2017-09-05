25d114340b0acba15e886f6553c03e3f7053dc93
Jens Tinfors ★ <jens.tinfors@gmail.com>
2017-09-05 14:08:34 +0200

    I really should've committed this when I finished it...

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+var kidsCounter = [
+	{ num: 0, textual: 'Inga' },
+	{ num: 1, textual: 'Ett' },
+	{ num: 2, textual: 'Två' },
+	{ num: 3, textual: 'Tre' },
+	{ num: 4, textual: 'Fyra' },
+];
+
+var familyCounter = [
+	{ num: 1, textual: 'En' },
+	{ num: 2, textual: 'Två' },
+	{ num: 3, textual: 'Tre' },
+	{ num: 4, textual: 'Fyra' },
+	{ num: 5, textual: 'Fem' },
+];
+
+var roomCounter = [
+	{ num: 1, textual: 'Ett' },
+	{ num: 2, textual: 'Två' },
+	{ num: 3, textual: 'Tre' },
+	{ num: 4, textual: 'Fyra' },
+	{ num: 5, textual: 'Fem' },
+];
+
 router.get('/', function(req, res, next) {
-	var familyMembers = [
-		{ num: 1, textual: 'En' },
-		{ num: 2, textual: 'Två' },
-		{ num: 3, textual: 'Tre' },
-		{ num: 4, textual: 'Fyra' },
-		{ num: 5, textual: 'Fem' },
-	]
-	res.render('index', { familyMembers: familyMembers, title: 'Tinfors Fastigheter' });
+	res.render('index', {
+		familyCounter: familyCounter,
+		kidsCounter: kidsCounter,
+		roomCounter: roomCounter,
+		title: 'Tinfors Fastigheter'
+	});
 });
 
 router.post('/', function(req, res, next) {
+	console.log(req.body);
 	res.render('success', { title: 'Success' });
 });
 

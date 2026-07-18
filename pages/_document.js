1d917b320cdcfe89e2879cf27a686813e4c41f3a
Jens Tinfors <jens.tinfors@svt.se>
2026-07-18 08:38:37 +0200

    Add Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { GoogleTagManager } from "@next/third-parties/google";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="sv">
       <Head>
-        <script src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_reCAPTCHA_site_key}`}></script>
+        <GoogleTagManager gtmId="GTM-K2M95RTN" />
+        <script
+          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_reCAPTCHA_site_key}`}
+        ></script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

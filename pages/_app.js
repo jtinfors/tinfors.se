54766ae4c292ab4944af6ede6a8bee022718c49e
Jens Tinfors <jens.tinfors@svt.se>
2026-07-19 09:45:00 +0200

    Add google analytics and tag manager to _app instead of _document

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '../styles/globals.css'
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleTagManager gtmId="GTM-K2M95RTN" />
+      <GoogleAnalytics gaId="AW-18318655303" />
+    </>
+  );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,9 @@
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="sv">
       <Head>
-        <GoogleTagManager gtmId="GTM-K2M95RTN" />
-        <GoogleAnalytics gaId="AW-18318655303" />
         <script
           src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_reCAPTCHA_site_key}`}
         ></script>

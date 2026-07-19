df6429cb70e287984ca09b1a1a56c12f67c96f92
Jens Tinfors <jens.tinfors@svt.se>
2026-07-19 09:36:51 +0200

    Fix broken production build in _document.js
    A stray unimported <Script> component was causing every page to fail
    prerendering (ReferenceError: Script is not defined), which is why
    Google couldn't detect the gtag on the live site. Removes the
    duplicate manual gtag.js/dataLayer scripts (GoogleAnalytics already
    renders that exact snippet) and restores the GTM container tag that
    had been dropped.

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,12 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="sv">
       <Head>
+        <GoogleTagManager gtmId="GTM-K2M95RTN" />
         <GoogleAnalytics gaId="AW-18318655303" />
-
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-18318655303"
-        ></Script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-18318655303')`,
-          }}
-        />
-
         <script
           src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_reCAPTCHA_site_key}`}
         ></script>
